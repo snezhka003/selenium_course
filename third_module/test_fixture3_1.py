227a6a210a6ab759748e2fcf14be87c3b76114cd
snezhka003 <you@example.com>
2023-12-28 13:41:32 +0300

    Окончание блока 4 третьего модуля

---
+++ third_module/test_fixture3_1.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser(request):
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    def teardown():
+        print("\nquit browser..")
+        browser.quit()
+
+    request.addfinalizer(teardown)
+    return browser
+
+
+class TestMainPage1():
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
